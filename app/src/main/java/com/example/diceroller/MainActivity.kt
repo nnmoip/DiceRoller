306198ff890280d639da6bf5c9ab480911ff25e5
Noellie Paulus <noelliepaulus@etu.univ-lyon1.fr>
2024-02-27 15:36:45 +0100

    Initialisation Dice Roller: Suppression des fonctions de démo et initialisation avec import images et bouton

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +34,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
     DiceWithButtonImage(
@@ -38,4 +46,22 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonImage(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
 }
